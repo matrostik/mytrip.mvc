df8cddbc6e5a89b076cd0bc9230007e9fbcc77c0
stuhin_cp <SND\stuhin_cp>
2010-09-30 19:26:56 +0000

    Branched from $/mytripmvc/Main/Source

---
+++ Development/cheedep/Source-branch/Mytrip.Mvc/Helpers/AvatarHelper.cs
@@ -0,0 +1,112 @@
+﻿/* Copyright © 2009 - 2010 Oleg Stuhin & Rostislav Haitovich 
+   To learn more about Mytrip.Mvc visit  
+   http://mytripmvc.net  http://mytripmvc.codeplex.com 
+   mytripmvc@gmail.com
+   license: Microsoft Public License (Ms-PL) */
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Mytrip.Mvc.Settings;
+
+namespace Mytrip.Mvc.Helpers
+{
+    /// <summary>ХТМЛ Разметка для аватара с использованием
+    /// глобальных аватаров от gravatar
+    /// </summary>
+    public static class AvatarHelper
+    {
+        /// <summary>Изображение Аватара пользователя
+        /// </summary>
+        /// <param name="html">HtmlHelper</param>
+        /// <param name="email">Email пользователя</param>
+        /// <returns>возвращает HtmlString</returns>
+        public static HtmlString Avatar(this HtmlHelper html, string email)
+        {
+            if (UsersSetting.unlockGravatar())
+            {
+                return GetImageTag(GetGravatar(email));
+            }
+            else { return null; }
+        }
+
+        /// <summary>Изображение Аватара пользователя
+        /// </summary>
+        /// <param name="html">HtmlHelper</param>
+        /// <param name="email">Email пользователя</param>
+        /// <param name="htmlAttributes">хтмл аттрибуты</param>
+        /// <returns>возвращает HtmlString</returns>
+        public static HtmlString Avatar(this HtmlHelper html, string email, object htmlAttributes)
+        {
+            if (UsersSetting.unlockGravatar())
+            {
+                return GetImageTag(GetGravatar(email), htmlAttributes);
+            }
+            else { return null; }
+        }
+
+        /// <summary>Изображение Аватара пользователя
+        /// </summary>
+        /// <param name="source">ссылка gravatar</param>
+        /// <returns>возвращает HtmlString</returns>
+        private static HtmlString GetImageTag(string source)
+        {
+            return GetImageTag(source, null);
+        }
+
+        /// <summary>Изображение Аватара пользователя
+        /// </summary>
+        /// <param name="source">ссылка gravatar</param>
+        /// <param name="htmlAttributes">хтмл аттрибуты</param>
+        /// <returns>возвращает HtmlString</returns>
+        private static HtmlString GetImageTag(string source, object htmlAttributes)
+        {
+
+            IDictionary<string, object> attributes =
+                (htmlAttributes == null
+                    ? new RouteValueDictionary()
+                    : new RouteValueDictionary(htmlAttributes));
+
+            string returnVal = "<img src=\"{0}\"";
+
+            foreach (string key in attributes.Keys)
+            {
+                returnVal += string.Format("{0}=\"{1}\"", key, attributes[key]);
+            }
+            returnVal += " />";
+            return new HtmlString(string.Format(returnVal, source));
+        }
+
+        /// <summary>ссылка gravatar
+        /// </summary>
+        /// <param name="email">Email пользователя</param>
+        /// <returns>возвращает string</returns>
+        private static string GetGravatar(string email)
+        {
+            return string.Format("http://www.gravatar.com/avatar/{0}", EncryptMD5(email));
+        }
+
+        /// <summary>Зашифровка Email пользователя
+        /// </summary>
+        /// <param name="Value">Email пользователя</param>
+        /// <returns>возвращает string</returns>
+        private static string EncryptMD5(string Value)
+        {
+            if (!String.IsNullOrEmpty(Value))
+            {
+                System.Security.Cryptography.MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                byte[] valueArray = System.Text.Encoding.ASCII.GetBytes(Value);
+                valueArray = md5.ComputeHash(valueArray);
+                string encrypted = "";
+                for (int i = 0; i < valueArray.Length; i++)
+                    encrypted += valueArray[i].ToString("x2").ToLower();
+                return encrypted;
+            }
+            else { return string.Empty; }
+        }
+    }
+}
+
+
---
+++ Development/cheedep/Source-branch/Mytrip.Mvc/Scripts/Mytrip.Mvc.Button.js
@@ -0,0 +1,64 @@
+﻿$(document).ready(function () {
+    $.ajax({ type: "POST", url: "/MytripMvc/Browser",
+        success: function (data) {
+            $('div.buttonIWL,div.buttonIWR').each(function () {
+                var a = $(this).html();
+                if (data == 'IE_7.0')
+                    var b = $(this).children('input').width();
+                $(this).html('<div class="buttonRW"/><div class="buttonLW"/><div class="buttonCW">' + a + '</div>');
+                if (data == 'IE_7.0') {
+                    var c = $('div.buttonLW').width();
+                    var d = $('div.buttonRW').width();
+                    if (b > 0)
+                        $(this).css({ width: ((b + c + d) + 'px') });
+                }
+            });
+            $('div.buttonIL,div.buttonIR').each(function () {
+                var a = $(this).html();
+                if (data == 'IE_7.0')
+                    var b = $(this).children('input').width();
+                $(this).html('<div class="buttonR"/><div class="buttonL"/><div class="buttonC">' + a + '</div>');
+                if (data == 'IE_7.0') {
+                    var c = $('div.buttonL').width();
+                    var d = $('div.buttonR').width();
+                    if (b > 0)
+                        $(this).css({ width: ((b + c + d) + 'px') });
+                }
+            });
+            $('div.buttonAWL,div.buttonAWR').each(function () {
+                var a = $(this).html();
+                if (data == 'IE_7.0')
+                    var b = $(this).children('a').width();
+                $(this).html('<div class="buttonRW"/><div class="buttonLW"/><div class="buttonCW">' + a + '</div>');
+                if (data == 'IE_7.0') {
+                    var c = $('div.buttonLW').width();
+                    var d = $('div.buttonRW').width();
+                    if (b > 0)
+                        $(this).css({ width: ((b + c + d) + 'px') });
+                }
+            });
+            $('div.buttonAL,div.buttonAR').each(function () {
+                var a = $(this).html();
+                if (data == 'IE_7.0')
+                    var b = $(this).children('a').width();
+                $(this).html('<div class="buttonR"/><div class="buttonL"/><div class="buttonC">' + a + '</div>');
+                if (data == 'IE_7.0') {
+                    var c = $('div.buttonL').width();
+                    var d = $('div.buttonR').width();
+                    if (b > 0)
+                        $(this).css({ width: ((b + c + d) + 'px') });
+                }
+            });
+        }
+    });
+    $('div.buttonIL,div.buttonIR,div.buttonAL,div.buttonAR').hover(function () {
+        $(this).find('div.buttonC,div.buttonL,div.buttonR').addClass("ac");
+    }, function ()
+    { $(this).find('div.buttonC,div.buttonL,div.buttonR').removeClass("ac"); });
+    $('div.buttonIWL,div.buttonIWR,div.buttonAWL,div.buttonAWR').hover(function () {
+        $(this).find('div.buttonCW,div.buttonLW,div.buttonRW').addClass("ac");
+    }, function () {
+        $(this).find('div.buttonCW,div.buttonLW,div.buttonRW').removeClass("ac");
+    });
+});
+/* Copyright © 2009 - 2010 Oleg Stuhin & Rostislav Haitovich To learn more about Mytrip.Mvc visit http://mytripmvc.net  http://mytripmvc.codeplex.com mytripmvc@gmail.com license: Microsoft Public License (Ms-PL)*/
